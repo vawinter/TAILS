ac00254bf417b8599d0f8af7dd9a3f5a60e6c230
vermanica <veronicaann621@gmail.com>
2022-10-25 13:14:54 -0400

    adding data for tracking

--- australiaScript.R
+++ australiaScript.R
@@ -7,11 +7,11 @@ cat <- getMovebankData("Red Fox (Vulpes vulpes) - Scotia, NSW, Australia", inclu
 fox <- getMovebankData("Feral cat (Felis catus) - Scotia, NSW", includeExtraSensors = TRUE)
 # https://cran.r-project.org/web/packages/move/index.html
 
-# visualization package options:
-https://movevis.org
-https://rstudio.github.io/leaflet/
-https://towardsdatascience.com/animating-your-data-visualizations-like-a-boss-using-r-f94ae20843e3
-https://hansenjohnson.org/post/animate-movement-in-r/
-https://www.datanovia.com/en/blog/gganimate-how-to-create-plots-with-beautiful-animation-in-r/
-https://plotly.com/r/
-  
+# # visualization package options:
+# https://movevis.org
+# https://rstudio.github.io/leaflet/
+# https://towardsdatascience.com/animating-your-data-visualizations-like-a-boss-using-r-f94ae20843e3
+# https://hansenjohnson.org/post/animate-movement-in-r/
+# https://www.datanovia.com/en/blog/gganimate-how-to-create-plots-with-beautiful-animation-in-r/
+# https://plotly.com/r/
+#   
