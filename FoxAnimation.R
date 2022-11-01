20e640c5a5e69fc4b63fb636006e4d281f851ad4
k-beatty <karenbrzezi@gmail.com>
2022-11-01 13:45:47 -0400

    Added path trace, aligned to 6hr interval.

--- FoxAnimation.R
+++ FoxAnimation.R
@@ -25,9 +25,10 @@ foxChoice <- fox %>%
   print() # aiming for inds w/ > 730 locs; all inds fit this qualification
 foxList <- foxChoice$local_identifier
 
-# Subset main data set to only the individuals and locations in 2017
+# Subset main data set to only the individuals and locations in 2017 Aug-Nov
 subFox <- fox[[foxList]]
 subFox <- subFox[year(timestamps(subFox)) == 2017]
+subFox <- subFox[month(timestamps(subFox)) %in% c(8, 9, 10, 11)]
 
 # Confirm timestamps and ids in specified range
 range(timestamps(subFox)) #    PROBLEM :: time range 7/31-11/21
@@ -42,17 +43,18 @@ library(RColorBrewer)
 colChoice <- brewer.pal(n = 9, name= 'Set1')
 
 # Display locations on interactive map
-view_spatial(moveFox, path_colours = colChoice, )
+view_spatial(moveFox, path_colours = colChoice)
 
 # Create animated movement path
-framesFox <- frames_spatial(moveFox, path_colours = colChoice, #   PROBLEM :: each path is multi-color - related to the palette call or tail_colour() ?
-                            map_service = 'osm', map_type = 'terrain_bg',
+framesFox <- frames_spatial(moveFox, path_colours = colChoice, # Can't figure out how to match tail colour to path colour by individual
+                            trace_show = TRUE, trace_colour = 'white',
+                            map_service = 'osm', map_type = 'watercolor',
                             alpha = 0.5) %>% 
   add_labels(x = "Longitude", y = "Latitude") %>%
   add_northarrow() %>%
   add_scalebar() %>%
   add_timestamps(type = "label") %>%
-  add_progress() %>%
-  add_gg(gg = expr(list(xlim(c(140.0, 142.0)), ylim(c(-30.0, -34.0)))))
+  add_progress()
+  # %>% add_gg(gg = expr(list(xlim(c(140.0, 142.0)), ylim(c(-30.0, -34.0))))) # Can't figure out how to manually change the map extent
 framesFox[[11]]
 animate_frames(framesFox, out_file = "foxVis.gif")
